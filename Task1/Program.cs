a6f5749bff0072b223496d0395e307fdfa100ab6
Vladimir-Kharchenko3 <141916443+Vladimir-Kharchenko3@users.noreply.github.com>
2023-12-04 09:58:16 +0300

    Решил задачу 1

---
+++ Task1/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true) // Бесконечный цикл
+        {
+            Console.Write("Введите число или 'q' для выхода: ");
+            string input = Console.ReadLine(); // Чтение строки ввода пользователя
+
+            if (input == "q") // Проверка на ввод 'q' для выхода
+            {
+                Console.WriteLine("[STOP]");
+                break;
+            }
+
+            if (int.TryParse(input, out int number)) // Проверка, является ли ввод числом
+            {
+                int sum = 0;
+                int originalNumber = Math.Abs(number);
+
+                while (originalNumber > 0) // Вычисление суммы цифр числа
+                {
+                    sum += originalNumber % 10; // Добавление последней цифры к сумме
+                    originalNumber /= 10; // Удаление последней цифры из числа
+                }
+
+                if (sum % 2 == 0) // Проверка, является ли сумма цифр четной
+                {
+                    Console.WriteLine("[STOP]");
+                    break;
+                }
+            }
+            else // Если ввод не является числом и не 'q', повторить запрос
+            {
+                Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число или 'q'.");
+            }
+        }
+    }
+}
